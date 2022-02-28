eef0dfe2286b9f7cd1190b0ac6cd4daa5dd3c642
omar-hardouf <55916713+omar-hardouf@users.noreply.github.com>
2022-02-28 15:49:18 +0000

    commint

--- docs/page1.fsx
+++ docs/page1.fsx
@@ -5,18 +5,19 @@
 printfn "Stock prices download example:\n"
 (***include-fsi-output***)
 
-#loar "YahooFinance.fsx"
+#load "YahooFinance.fsx"
 
 open YahooFinance
 
 let vti = YahooFinance.PriceHistory("vti") // 
 
-#r "nuget: Ploty.NET, 2.0.0-preview.17"
+#r "nuget: Plotly.NET, 2.0.0-preview.17"
 
-open Ploty.NativeInterop
+open Plotly.NET
 
-vti|> List.map(fun x -> x-x.Date, x.AdjustedClose)
+vti
+|> List.map(fun x -> x-x.Date, x.AdjustedClose)
 |> Chart.Line
 |> GenericChart.toChartHTML
 
-(***include***)
+(***include-it-raw***)
