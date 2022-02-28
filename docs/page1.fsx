90ce68be8179b82dce77d6d67c9ad4fe51cbc9d7
omar-hardouf <55916713+omar-hardouf@users.noreply.github.com>
2022-02-28 15:42:00 +0000

    Adding stock charts

--- docs/page1.fsx
+++ docs/page1.fsx
@@ -2,5 +2,21 @@
 ## This is my first page
 *)
 
-printfn "Im bored"
+printfn "Stock prices download example:\n"
 (***include-fsi-output***)
+
+#loar "YahooFinance.fsx"
+
+open YahooFinance
+
+let vti = YahooFinance.PriceHistory("vti") // 
+
+#r "nuget: Ploty.NET, 2.0.0-preview.17"
+
+open Ploty.NativeInterop
+
+vti|> List.map(fun x -> x-x.Date, x.AdjustedClose)
+|> Chart.Line
+|> GenericChart.toChartHTML
+
+(***include***)
